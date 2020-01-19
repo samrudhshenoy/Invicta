f7cc21a8af33b173566670360172c1f9f0146286
RajRaghulan <rajraghulan@gmail.com>
2020-01-19 08:02:48 -0800

    Changes

--- js/shop.js
+++ js/shop.js
@@ -1,8 +1,14 @@
 var addCartItems = document.getElementsByClassName('add-to-cart')
 console.log(addCartItems)
+var counter = 0;
 for (var i = 0; i<addCartItems.length; i++){
 	var button = addCartItems[i]
 	button.addEventListener('click', function(event){
-		var buttonClicked = event.target
+		console.log('clicked')
+		counter++;
+		console.log(counter)
+  		document.getElementById("test").innerHTML = counter; 
 	})
 }
+
+
