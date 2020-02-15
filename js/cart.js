8dd09d0561cb4a0f79e70b95539d7d88d31390fd
RajRaghulan <32783514+RajRaghulan@users.noreply.github.com>
2020-02-15 09:20:38 -0800

    New Version of Cart

--- js/cart.js
+++ js/cart.js
@@ -1,42 +1,43 @@
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready);
 } else {
-    ready()
+    ready();
 }
 
 function ready() {
-
-    var removeCartItemButtons = document.getElementsByClassName('product-remove')
-    console.log(removeCartItemButtons)
+    var itemsInCart = document.getElementsByClassName("text-center");
+    document.getElementById("myText").innerHTML = itemsInCart;
+    var removeCartItemButtons = document.getElementsByClassName("product-remove");
+    console.log(removeCartItemButtons);
     for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
+        var button = removeCartItemButtons[i];
+        button.addEventListener("click", removeCartItem);
     }
-
 }
 
 function removeCartItem(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal()
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateItemsInCart();
+    updateCartTotal();
 }
 
 function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('table')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('text-center')
-    var total = 0
+    var cartItemContainer = document.getElementsByClassName("table")[0];
+    var cartRows = cartItemContainer.getElementsByClassName("text-center");
+    var total = 0;
     var oneTotal = 0;
     for (var i = 1; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('price')[0]
-        var quantityElement = cartRow.getElementsByClassName('quantity')[0]
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName("price")[0];
+        var quantityElement = cartRow.getElementsByClassName("quantity")[0];
+        var price = parseFloat(priceElement.innerText.replace("$", ""));
 
-        var quantity = quantityElement.getElementsByClassName('quantity')[0]
-        var realQuantity = quantity.value
-        oneTotal = price * realQuantity
-        cartRow.getElementsByClassName('total')[0].innertext = '$' + oneTotal
-        total = total + (price * realQuantity)
+        var quantity = quantityElement.getElementsByClassName("quantity")[0];
+        var realQuantity = quantity.value;
+        oneTotal = price * realQuantity;
+        cartRow.getElementsByClassName("total")[0].innertext = "$" + oneTotal;
+        total = total + price * realQuantity;
     }
-    document.getElementsByClassName('total-price')[0].innerText = '$' + total
+    document.getElementsByClassName("total-price")[0].innerText = "$" + total;
 }
