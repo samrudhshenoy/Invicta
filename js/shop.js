5b67938edc8f39259d2bbed44eb666e804eec799
RajRaghulan <rajraghulan@gmail.com>
2020-02-21 23:09:02 -0800

    Got Google Maps to Work

--- js/shop.js
+++ js/shop.js
@@ -1,14 +1,12 @@
 var addCartItems = document.getElementsByClassName('add-to-cart')
 console.log(addCartItems)
 var counter = 0;
-for (var i = 0; i<addCartItems.length; i++){
-	var button = addCartItems[i]
-	button.addEventListener('click', function(event){
-		console.log('clicked')
-		counter++;
-		console.log(counter)
-  		document.getElementById("test").innerHTML = counter; 
-	})
+for (var i = 0; i < addCartItems.length; i++) {
+    var button = addCartItems[i]
+    button.addEventListener('click', function(event) {
+        console.log('clicked')
+        counter++;
+        console.log(counter)
+        document.getElementById("test").innerHTML = counter;
+    })
 }
-
-
