f42386a4f4a1d53c65bbd989edb18b40e75b7c54
RajRaghulan <rajraghulan@gmail.com>
2020-02-22 19:30:26 -0800

    Counter fix

--- js/shop.js
+++ js/shop.js
@@ -1,12 +1,14 @@
-var addCartItems = document.getElementsByClassName('add-to-cart')
-console.log(addCartItems)
+var addCartItems = document.getElementsByClassName("add-to-cart");
+console.log(addCartItems);
 var counter = 0;
+document.getElementById("counter").innerHTML = counter;
+
 for (var i = 0; i < addCartItems.length; i++) {
-    var button = addCartItems[i]
-    button.addEventListener('click', function(event) {
-        console.log('clicked')
+    var button = addCartItems[i];
+    button.addEventListener("click", function(event) {
+        console.log("clicked");
         counter++;
-        console.log(counter)
-        document.getElementById("test").innerHTML = counter;
-    })
+        console.log(counter);
+        document.getElementById("counter").innerHTML = counter;
+    });
 }
